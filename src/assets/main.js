9a1289aae45106b8918427007940726654c412a6
BaNburger <b.burger@gmx.eu>
2016-12-17 22:16:04 +0100

    All of it

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,35 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2492580.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addcourses(response.courses.completed);
+    }
+  });
+
+  function addcourses(courses) {
+    var $badges = $('#badges');
+    courses.forEach(function(course) {
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges)
+
+      var $title = $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      var $image = $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      var $anchor = $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
+    })
+  }
 
 });
